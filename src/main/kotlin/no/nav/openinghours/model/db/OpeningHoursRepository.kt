19d26327c6cf3e890e04383d9a53d958f095cba3
OrleneMitchell <88720049+OrleneMitchell@users.noreply.github.com>
2026-04-13 14:56:31 +0200

    Change ID type from String to UUID in repository
    Updated OpeningHoursRepository to use UUID as the ID type.

--- src/main/kotlin/no/nav/openinghours/model/db/OpeningHoursRepository.kt
+++ src/main/kotlin/no/nav/openinghours/model/db/OpeningHoursRepository.kt
@@ -3,13 +3,7 @@ package no.nav.openinghours.model.db
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
-interface OpeningHoursRepository : JpaRepository<OpeningHours, String> {
-    fun findById(id: UUID): OpeningHours?
-
+interface OpeningHoursRepository : JpaRepository<OpeningHours, UUID> {
+    
     fun findByName(name: String): OpeningHours?
-
-    fun deleteById(id: UUID)
-
-    fun existsById(id: UUID): Boolean
-
 }
