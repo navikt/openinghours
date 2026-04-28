d495fde590b22064aa86f5bce2a40012eb5ee71d
M137316 <orlene.mitchell@nav.no>
2026-04-28 14:12:04 +0200

    remove code

--- src/main/kotlin/no/nav/openinghours/Main.kt
+++ src/main/kotlin/no/nav/openinghours/Main.kt
@@ -7,7 +7,6 @@ import org.springframework.boot.runApplication
 class Openinghours
 
 fun main(args: Array<String>) {
-    println("DB_url: ${System.getenv("DB_JDBC_URL")}")
     println("DB_DATABASE: ${System.getenv("DB_DATABASE")}")
     println("DB_USERNAME: ${System.getenv("DB_USERNAME")}")
         runApplication<Openinghours>(*args)
