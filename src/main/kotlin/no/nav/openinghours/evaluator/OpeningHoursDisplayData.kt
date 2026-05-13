28dfea53bb4b9da3afba44861fe24745be076833
M137316 <orlene.mitchell@nav.no>
2026-05-13 12:02:43 +0200

    Addition the Rule evaluation engine (OpeningHoursParser port) w

---
+++ src/main/kotlin/no/nav/openinghours/evaluator/OpeningHoursDisplayData.kt
@@ -0,0 +1,8 @@
+package no.nav.openinghours.evaluator
+
+data class OpeningHoursDisplayData(
+    val ruleName: String? = null,
+    val rule: String? = null,
+    val openingHours: String? = null,
+    val displayText: String? = null,
+)
